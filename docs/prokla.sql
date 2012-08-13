9723f43527c26b311e417c9cb2c87c7ddbb6917a
prokla <juscelino@redel.com.br>
2012-08-13 18:26:40 -0300

    SQL

--- docs/prokla.sql
+++ docs/prokla.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: 11/08/2012 às 23h36min
+-- Tempo de Geração: 13/08/2012 às 18h26min
 -- Versão do Servidor: 5.1.63
 -- Versão do PHP: 5.3.14
 
@@ -33,9 +33,34 @@ CREATE TABLE IF NOT EXISTS `ads` (
   `id_user` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `text` text NOT NULL,
+  `price` decimal(9,2) NOT NULL DEFAULT '0.00',
+  `image` varchar(255) NOT NULL DEFAULT 'null.jpg',
   PRIMARY KEY (`id`),
   KEY `id_user` (`id_user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=35 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `ads`
+--
+
+INSERT INTO `ads` (`id`, `id_user`, `title`, `text`, `price`, `image`) VALUES
+(1, 1, 'sd fasd', 'fasd fasd fasd', '0.00', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE IF NOT EXISTS `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_ads` int(11) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_ads` (`id_ads`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -50,7 +75,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+(1, 'Jus', 'jus@jus.com', '6ecd178dec5ed191b3d7911eabe7b75e');
 
 --
 -- Restrições para as tabelas dumpadas
@@ -61,6 +93,12 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 ALTER TABLE `ads`
   ADD CONSTRAINT `ads_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para a tabela `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`id_ads`) REFERENCES `ads` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 SET FOREIGN_KEY_CHECKS=1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
